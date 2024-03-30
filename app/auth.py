d3491fd3f470eec62af1e92ba9d38f5b36e91436
J Cui <i@cjxol.com>
2024-03-30 16:55:38 +0000

    Format code

--- app/auth.py
+++ app/auth.py
@@ -14,8 +14,9 @@ bp = Blueprint('auth', __name__)
 
 google_bp = None
 
+
 def register_google_bp(app):
-    global google_bp # NOT GOOD
+    global google_bp  # NOT GOOD
     google_bp = make_google_blueprint(
         client_id=secrets['client_id'],
         client_secret=secrets['client_secret'],
@@ -29,6 +30,7 @@ def index():
     if google.authorized:
         return redirect(url_for('auth.profile'))
     return render_template('auth/login.html')
+
 
 @bp.route('/logout')
 def logout():
@@ -48,6 +50,7 @@ def logout():
     del google_bp.token  # Delete OAuth token from storage
     return redirect(url_for('auth.index'))
 
+
 def login_required(view):
     """View decorator that redirects anonymous users to the login page."""
     @functools.wraps(view)
@@ -58,31 +61,31 @@ def login_required(view):
 
     return wrapped_view
 
+
 @bp.route('/profile')
 @login_required
 def profile():
     # hacky, but this pins the google profile information to the session
     resp = requests.get(
-            'https://people.googleapis.com/v1/people/me',
-            params={
-                'personFields': 'names,emailAddresses,photos'
-            },
-            headers={
-                'Authorization': 'Bearer {}'.format(google.token[u'access_token'])
-            })
+        'https://people.googleapis.com/v1/people/me',
+        params={
+            'personFields': 'names,emailAddresses,photos'
+        },
+        headers={
+            'Authorization': 'Bearer {}'.format(google.token[u'access_token'])
+        })
 
     person_info = resp.json()
 
     profile = {
         'email': person_info[u'emailAddresses'][0][u'value'],
         'name': person_info[u'names'][0][u'displayName'],
-        'image': person_info[u'photos'][0][u'url'].split('=')[0] # remove the 100px limit (ends with =s100)
+        # remove the 100px limit (ends with =s100)
+        'image': person_info[u'photos'][0][u'url'].split('=')[0]
     }
 
     session['name'] = profile['name']
     session['email'] = profile['email']
     session['image'] = profile['image']
 
-    
     return redirect(url_for('dashboard.index'))
-
