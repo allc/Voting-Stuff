7df527c8222412b9a29f0605d8fd3c46b6b8ad21
J Cui <i@cjxol.com>
2024-04-04 00:54:41 +0100

    Format

--- app/db.py
+++ app/db.py
@@ -12,9 +12,9 @@ def get_db():
     """
     if 'db' not in g:
         g.db = sqlite3.connect(
-                current_app.config['DATABASE'],
-                detect_types=sqlite3.PARSE_DECLTYPES
-                )
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
         g.db.row_factory = sqlite3.Row
 
     g.db.execute('PRAGMA foreign_keys = ON')
